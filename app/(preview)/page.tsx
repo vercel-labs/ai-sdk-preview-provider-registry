c5f0854425d245e4354c5e467b0ab43e8db14442
jeremyphilemon <git@jrmyphlmn.com>
2024-09-05 01:11:06 +0530

    Add deploy buttons

--- app/(preview)/page.tsx
+++ app/(preview)/page.tsx
@@ -5,6 +5,7 @@ import { Message } from "@/components/message";
 import { useScrollToBottom } from "@/components/use-scroll-to-bottom";
 import { AnimatePresence, motion } from "framer-motion";
 import {
+  GitIcon,
   LogoAnthropic,
   LogoGoogle,
   LogoOpenAI,
@@ -211,6 +212,31 @@ export default function Home() {
           </>
         )}
       </AnimatePresence>
+
+      <motion.div
+        className="flex flex-row gap-4 items-center justify-between fixed bottom-6 text-xs "
+        initial={{ y: 20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ delay: 1 }}
+      >
+        <Link
+          target="_blank"
+          href="https://github.com/vercel-labs/ai-sdk-preview-provider-registry"
+          className="flex flex-row gap-2 items-center border px-2 py-1.5 rounded-md hover:bg-zinc-100 dark:border-zinc-700 dark:hover:bg-zinc-800"
+        >
+          <GitIcon />
+          View Source Code
+        </Link>
+
+        <Link
+          target="_blank"
+          href="https://vercel.com/templates/next.js/ai-sdk-provider-registry"
+          className="flex flex-row gap-2 items-center bg-zinc-900 px-2 py-1.5 rounded-md text-zinc-50 hover:bg-zinc-950 dark:bg-zinc-100 dark:text-zinc-900 dark:hover:bg-zinc-50"
+        >
+          <VercelIcon size={14} />
+          Deploy with Vercel
+        </Link>
+      </motion.div>
     </div>
   );
 }
