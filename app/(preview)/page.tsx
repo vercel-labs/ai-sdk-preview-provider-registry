9eb6a40142e0c556fdf4494672d8b30af1609386
jeremyphilemon <git@jrmyphlmn.com>
2024-09-05 14:33:44 +0530

    Remove unused message component

--- app/(preview)/page.tsx
+++ app/(preview)/page.tsx
@@ -104,7 +104,6 @@ export default function Home() {
               key={message.id}
               role={message.role}
               content={message.content}
-              toolInvocations={message.toolInvocations}
             ></Message>
           ))}
           <div ref={messagesEndRef} />
