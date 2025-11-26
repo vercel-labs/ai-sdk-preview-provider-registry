d6007ab6201bec025a67503d103166eb828453c8
Hayden Bleasel <hello@haydenbleasel.com>
2025-11-26 14:46:00 -0800

    Update tailwind.config.ts
    Co-authored-by: vercel[bot] <35613825+vercel[bot]@users.noreply.github.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -75,11 +75,11 @@ const config: Config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
-    },
-    backgroundImage: {
-      "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-      "gradient-conic":
-        "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
     },
   },
   plugins: [animate],
